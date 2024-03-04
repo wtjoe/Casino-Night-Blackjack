30cdc7c014c2ad0452599359f2f29adc3bb18350
Joe Robbins <joe.robbins@willowtreeapps.com>
2024-03-04 11:34:51 -0500

    POC of socket.io

--- client/App.tsx
+++ client/App.tsx
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { io } from "socket.io-client";
+
+const socket = io();
 
 const App: React.FC = () => {
+  React.useEffect(() => {
+    console.log('Connecting to WebSocket');
+    socket.connect();
+
+    return () => {
+      console.log('Disconnecting from WebSocket');
+      socket.disconnect();
+    };
+  }, []);
+
   return <h1>TypeScript is awesome</h1>;
 };
 
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,25 @@
 import Game from './classes/game'
 import Table from './classes/table'
 import Player from './classes/player'
-import express, { Request, Response, NextFunction } from "express";
-import path from "path";
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+import express, { Express, Request, Response, NextFunction } from "express"
+import path from "path"
 
 const game = new Game('Blackjack')
 const table = new Table()
-const player1 = new Player('Player1', 1000)
-const player2 = new Player('Player2', 1000)
-table.addPlayer(player1)
-table.addPlayer(player2)
-game.deal(table)
 
-game.resolve(table)
+// const player1 = new Player('Player1', 1000)
+// const player2 = new Player('Player2', 1000)
+// table.addPlayer(player1)
+// table.addPlayer(player2)
+// game.deal(table)
 
-const app = express();
+// game.resolve(table)
+
+const app: Express = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
 
 app.use(express.static(path.join(__dirname, "../public")));
 
@@ -26,8 +31,16 @@ app.get("/", (req: Request, res: Response, next: NextFunction): void => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+io.on('connection', (socket) => {
+  console.log(('New Websocket connection'))
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+})
+
+httpServer.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
